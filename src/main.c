0ea13b4ae9b24577ce22d5c121e8a247ee09fbe4
Finlay Davidson <finlay.davidson@coderclass.nl>
2022-06-21 14:26:29 +0200

    Minor changes to error handling and add exe to gitignore

--- src/main.c
+++ src/main.c
@@ -1,37 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+#include <glad/gl.h>
+
 #include <standard_types.h>
 
-#include <glad/gl.h>
-#include <GLFW/glfw3.h>
+#define GL_VERSION_MAJOR 4
+#define GL_VERSION_MINOR 6
+#define WINDOW_WIDTH 1024
+#define WINDOW_HEIGHT 1024
 
-int main(){
-    if (!glfwInit()){
-        printf("Failed to init glfw!\n");
-        exit(1);
+static void error_callback(int error, const char* description) {
+    fprintf(stderr, "Error: %d %s\n", error, description);
+}
+
+int main() {
+    glfwSetErrorCallback(error_callback);
+    if (!glfwInit()) {
+        return 1;
     }
 
-    uint32_t window_width = 1024;
-    uint32_t window_height = 1024;
-    GLFWwindow* window = glfwCreateWindow(window_width, window_height, "Volumetric Raytracer", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH,
+                                          WINDOW_HEIGHT,
+                                          "Volumetric Raytracer",
+                                          NULL,
+                                          NULL);
+    if (!window) {
+        glfwTerminate();
+        return 2;
+    }
 
     glfwMakeContextCurrent(window);
     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, GL_VERSION_MAJOR);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, GL_VERSION_MINOR);
 
-    if (!gladLoadGL((GLADloadfunc)glfwGetProcAddress)){
-        printf("Failed to init glad!\n");
-        exit(1);
+    if (!gladLoadGL(glfwGetProcAddress)) {
+        glfwDestroyWindow(window);
+        glfwTerminate();
+        return 3;
     }
 
-    while(!glfwWindowShouldClose(window)){
-        
-        
+    while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
         glfwSwapBuffers(window);
     }
     glfwDestroyWindow(window);
     glfwTerminate();
+
+    return 0;
 }
