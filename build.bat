c1b238b7668bd0db55f7308bae58bfe362cc3056
Finlay Davidson <finlay.davidson@coderclass.nl>
2022-10-18 18:03:27 +0200

    Fix incorrect filenames

--- build.bat
+++ build.bat
@@ -1,26 +1,26 @@
 :: gcc src/main.c src/shader.c src/texture.c src/logging.c src/gl.c glfw3.dll -o main.exe -I include -std=c17 -Wall -Wextra -Wpedantic
 
-gcc src/main.c -c -I include -I cimgui -std=c17 -Wall -Wextra -Wpedantic -o object_files/main.o
-gcc src/shader.c -c -I include -I cimgui -std=c17 -Wall -Wextra -Wpedantic -o object_files/shader.o
-gcc src/texture.c -c -I include -I cimgui -std=c17 -Wall -Wextra -Wpedantic -o object_files/texture.o
-gcc src/logging.c -c -I include -I cimgui -std=c17 -Wall -Wextra -Wpedantic -o object_files/logging.o
-gcc src/gl.c -c -I include -I cimgui -std=c17 -Wall -Wextra -Wpedantic -o object_files/gl.o
-gcc src/bmp_imp.c -c -I include -I cimgui -std=c17 -Wall -Wextra -Wpedantic -o object_files/bmp_imp.o
+gcc src/main.c -c -I include -I cimgui -std=c17 -Wall -Wextra -Wpedantic -o obj/main.o
+gcc src/shader.c -c -I include -I cimgui -std=c17 -Wall -Wextra -Wpedantic -o obj/shader.o
+gcc src/texture.c -c -I include -I cimgui -std=c17 -Wall -Wextra -Wpedantic -o obj/texture.o
+gcc src/logging.c -c -I include -I cimgui -std=c17 -Wall -Wextra -Wpedantic -o obj/logging.o
+gcc src/gl.c -c -I include -I cimgui -std=c17 -Wall -Wextra -Wpedantic -o obj/gl.o
+gcc src/bmp_imp.c -c -I include -I cimgui -std=c17 -Wall -Wextra -Wpedantic -o obj/bmp_imp.o
 
 g++ ^
-object_files/main.o ^
-object_files/shader.o ^
-object_files/texture.o ^
-object_files/logging.o ^
-object_files/gl.o ^
-object_files/bmp_imp.o ^
-cimgui/cimgui.o ^
-cimgui/imgui.o ^
-cimgui/imgui_demo.o ^
-cimgui/imgui_draw.o ^
-cimgui/imgui_impl_glfw.o ^
-cimgui/imgui_impl_opengl3.o ^
-cimgui/imgui_tables.o ^
-cimgui/imgui_widgets.o ^
+obj/main.o ^
+obj/shader.o ^
+obj/texture.o ^
+obj/logging.o ^
+obj/gl.o ^
+obj/bmp_imp.o ^
+obj/cimgui.o ^
+obj/imgui.o ^
+obj/imgui_demo.o ^
+obj/imgui_draw.o ^
+obj/imgui_impl_glfw.o ^
+obj/imgui_impl_opengl3.o ^
+obj/imgui_tables.o ^
+obj/imgui_widgets.o ^
 -lglfw3 ^
 -o main.exe
